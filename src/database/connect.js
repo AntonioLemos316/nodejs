ce3fb0b7f4859b7aca7e71c7eacddd96ebe6bcf5
Antonio Marques <tonysenha@gmail.com>
2023-12-06 00:29:03 -0300

    Alteração de uma const que recebera uma url

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 
+const URL = '@cursonodejs.k66k1no.mongodb.net/database?retryWrites=true&w=majority'
+
 const connectToDatabase = async () => {
-    await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cursonodejs.k66k1no.mongodb.net/database?retryWrites=true&w=majority`)
+    await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}${URL}`)
     .then(console.log('Conexão efetuada com sucesso'))
     .catch(error => {
         console.log('Ocorreu um erro ao relizar a conexão! Erro: ', error)
